a0ade1878388f93f5bf89877f6b2f007cffff03e
fanny <fanny@PC-FANNY>
2024-09-02 12:12:33 +0200

    Phase 2 : premier test "Hello world!"

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AccueilController
+ *
+ * @author fanny
+ */
+class AccueilController {
+   
+    #[Route('/', name: 'accueil')]
+    public function index(): Response{
+        return new Response('Hello world !');
+    }
+    
+}
